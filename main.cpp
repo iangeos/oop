b097c98dc3a57d5b9c40ba12ea47607d1a68fdb9
air <air@airs-MacBook-Air.local>
2018-04-08 22:28:40 +0200

    adding main

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std; 
+
+int main()
+{
+	Circle c1(4.0);
+	cout << "Hello!" << endl;  
+	cout << "Area of the shape is: " << c1.area() << "\n";
+	cout << "Perimeter of the shape is: " << c1.perimeter() << endl;
+
+        Rectangle r1(4.0,5.0);
+        cout << "Hello!" << endl;
+        cout << "Area of the shape is: " << r1.area() << "\n";
+        cout << "Perimeter of the shape is: " << r1.perimeter() << endl;
+
+	int size = 3;
+	Shape *sarray[size];
+	sarray[0] = new Circle(4.0);
+	sarray[1] = new Circle(5.0);
+	sarray[2] = new Rectangle(4.0,5.0);
+	
+	for (int i=0; i<size; i++) {
+ 		cout << "Area of the shape is: " << sarray[i]->area() << endl;
+		cout << "Perimeter of the shape is: " << sarray[i]->perimeter() << endl;
+	}
+
+	return 0;
+}
